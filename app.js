1d8a00ed14ee09c6357f53db97b187dbb6cec9a0
Samuel <samuel.linehan15@gmail.com>
2016-05-12 11:19:08 -0400

    added server

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var Express = require("express");
 var app = Express();
+var server = require('http').Server(app)
 var request = require("request");
 var cors = require("cors");
 require("dotenv").config();
 
 app.use(cors());
 
+server.listen(process.env.PORT || 3000, function(){
+  console.log('listening')
+})
 
 app.get("/data", function(req, res) {
   request("http://api.openweathermap.org/data/2.5/forecast/city?id=4940625&APPID=" + process.env.WEATHER_API_KEY + "&units=imperial", function(error, response, body) {
@@ -16,5 +20,3 @@ app.get("/data", function(req, res) {
     }
   });
 })
-
-app.listen(3000);
